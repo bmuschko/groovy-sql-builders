0074cba2dfb3d6d71ed21c30303e2c0eea7f5dba
bmuschko <benjamin.muschko@gmail.com>
2011-02-27 10:07:37 -0500

    Deleted unused code.

--- src/test/groovy/groovy/sql/builder/GroovySqlHandler.groovy
+++ src/test/groovy/groovy/sql/builder/GroovySqlHandler.groovy
@@ -16,10 +16,6 @@
 package groovy.sql.builder
 
 import groovy.sql.Sql
-import javax.naming.Context
-import javax.naming.InitialContext
-import javax.sql.DataSource
-import org.h2.jdbcx.JdbcDataSource
 
 /**
  *
@@ -35,19 +31,5 @@ class GroovySqlHandler {
     static createDriverManagerSql(String url = DEFAULT_URL, String username = DEFAULT_USERNAME,
                                                  String password = DEFAULT_PASSWORD, String driverClassName = DEFAULT_DRIVER_CLASS_NAME) {
         Sql.newInstance(url, username, password, driverClassName)
-    }
-
-    static createDataSourceSql(url = DEFAULT_URL, username = DEFAULT_USERNAME, password = DEFAULT_PASSWORD) {
-        DataSource dataSource = new JdbcDataSource()
-        dataSource.URL = url
-        dataSource.user = username
-        dataSource.password = password
-        new Sql(dataSource)
-    }
-
-    static createJndiDataSourceSql(String jndiName) {
-        Context context = new InitialContext()
-        DataSource dataSource = context.lookup(jndiName)
-        new Sql(dataSource)
     }
 }
